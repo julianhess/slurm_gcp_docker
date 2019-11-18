8183da7670dabccf12d9b97a2853141f979cfec5
Julian Hess <jhess@broadinstitute.org>
2019-11-18 17:15:02 +0000

    Better error checking during NFS mount
    - Only creates/mounts NFS disk if it does not already exist/is not
      mounted
    - Attaches disk with useful name
    - Suppresses tee output of writing to /etc/exports

--- src/nfs_provision_server.sh
+++ src/nfs_provision_server.sh
@@ -25,15 +25,21 @@ ZONE=$(gcloud compute instances list --filter="name=${HOSTNAME}" \
   --format='csv[no-heading](zone)')
 
 #
-# create and attach NFS disk
+# create and attach NFS disk (if it does not already exist)
 echo -e "Creating NFS disk ...\n"
 
-gcloud compute disks create ${HOSTNAME}-nfs --size ${SIZE}GB --type $DISKTYPE \
-  --zone $ZONE
-gcloud compute instances attach-disk $HOSTNAME --disk ${HOSTNAME}-nfs --zone $ZONE
+gcloud compute disks list --filter="name=${HOSTNAME}-nfs" --format='csv[no-heading](type)' | \
+  grep -q $DISKTYPE || \
+  gcloud compute disks create ${HOSTNAME}-nfs --size ${SIZE}GB --type $DISKTYPE --zone $ZONE
+[ -b /dev/disk/by-id/google-${HOSTNAME}-nfs ] || \
+  { gcloud compute instances attach-disk $HOSTNAME --disk ${HOSTNAME}-nfs --zone $ZONE \
+      --device-name ${HOSTNAME}-nfs && \
+    gcloud compute instances set-disk-auto-delete $HOSTNAME --disk ${HOSTNAME}-nfs \
+      --zone $ZONE; }
 
 #
-# format NFS disk
+# format NFS disk (if it's not already mounted)
+mountpoint -q /mnt/nfs || {
 echo -e "\nFormatting disk ...\n"
 
 # XXX: we assume that this will always be /dev/sdb. In the future, if we are
@@ -46,13 +52,14 @@ echo -e "\nMounting disk ...\n"
 
 # this should already be present, but let's do this just in case
 [ ! -d /mnt/nfs ] && sudo mkdir -p /mnt/nfs
-sudo mount -o discard,defaults /dev/sdb /mnt/nfs
+sudo mount -o discard,defaults /dev/sdb /mnt/nfs;
+}
 
 #
 # add to exports; restart NFS server
 
-# XXX: this will preclude any preexisting NFS mounts defined in the base image.
-sudo tee /etc/exports <<< \
+# XXX: this will preclude having any preexisting NFS mounts defined in the base image.
+sudo tee /etc/exports > /dev/null <<< \
 "/mnt/nfs ${HOSTNAME}-worker*(rw,async,no_subtree_check,insecure,no_root_squash)"
 
 sudo service nfs-kernel-server restart
