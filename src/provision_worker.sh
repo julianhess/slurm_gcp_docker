0f918ac072ded6be64dee742fd5e5f9608720ad3
Julian Hess <jhess@broadinstitute.org>
2019-11-19 12:51:37 -0500

    Add hostname stem

--- src/provision_worker.sh
+++ src/provision_worker.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-/usr/local/share/cga_pipeline/src/nfs_provision_worker.sh
+/usr/local/share/cga_pipeline/src/nfs_provision_worker.sh $1
 sudo munged -f
 sudo slurmd -f /mnt/nfs/clust_conf/slurm/slurm.conf
