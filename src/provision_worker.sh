8596f5b3cb917bb0f48229db940ea71ffd88b286
Julian Hess <jhess@broadinstitute.org>
2019-11-20 10:48:22 -0500

    Need to export SLURM_CONF for slurmd
    Apparently, -f $SLURM_CONF ain't sufficient ...

--- src/provision_worker.sh
+++ src/provision_worker.sh
@@ -3,5 +3,8 @@
 set -e
 
 /usr/local/share/cga_pipeline/src/nfs_provision_worker.sh $1
+
+export SLURM_CONF=/mnt/nfs/clust_conf/slurm/slurm.conf
+
 sudo munged -f
-sudo slurmd -f /mnt/nfs/clust_conf/slurm/slurm.conf
+sudo -E slurmd -f /mnt/nfs/clust_conf/slurm/slurm.conf
