9b00869e1b3c953e59501b36522a14c216a7cf0b
Julian Hess <jhess@broadinstitute.org>
2019-12-12 15:38:29 -0500

    tmp

--- src/docker_entrypoint.sh
+++ src/docker_entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-. ~/.bashrc
+#. ~/.bashrc
 
 [ ! -d /run/sendsigs.omit.d ] && sudo mkdir -p /run/sendsigs.omit.d
 sudo service rpcbind restart
 
 mysqld &
 cd /usr/local/share/cga_pipeline/src
-./provision_server.py
+#./provision_server.py
