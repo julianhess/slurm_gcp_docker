111410e5b7fa8960369124e9a0d7256c600ed99d
Julian Hess <jhess@broadinstitute.org>
2019-12-12 15:45:08 -0500

    Explicitly source gcloud path update script

--- src/docker_entrypoint.sh
+++ src/docker_entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#. ~/.bashrc
+. /root/google-cloud-sdk/path.bash.inc
 
 [ ! -d /run/sendsigs.omit.d ] && sudo mkdir -p /run/sendsigs.omit.d
 sudo service rpcbind restart
 
 mysqld &
 cd /usr/local/share/cga_pipeline/src
-#./provision_server.py
+./provision_server.py
 /bin/bash
