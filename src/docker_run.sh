b652d2fcc8018f5f580e1aea84cd5b173876e08e
Julian Hess <jhess@broadinstitute.org>
2019-12-12 18:20:24 -0500

    Get rid of nfs_make_disk.sh
    Since we're not running NFS in the Docker anymore, this code only gets called
    once

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-#
-# create, attach, format, and mount NFS disk (if these things have not yet already
-# been done)
-. nfs_make_disk.sh
-
 sudo docker run --privileged --rm --network host -ti -v /mnt/nfs:/mnt/nfs --name "pype_host" broadinstitute/pydpiper /bin/bash
