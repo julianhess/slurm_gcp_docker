663f75a3be09d023da1103e8ebf17b6d78c67f1a
Julian Hess <jhess@broadinstitute.org>
2019-12-13 15:48:23 -0500

    Don't bother with bind mount; just mount from NFS server directly

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --privileged --rm --network host -ti -v /mnt/nfs:/mnt/nfs --name "pype_host" broadinstitute/pydpiper /bin/bash
+sudo docker run --privileged --rm --network host -ti --name "pype_host" broadinstitute/pydpiper /bin/bash
