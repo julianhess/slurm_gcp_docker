83ea1b7d5a135163b9b8a2f49b2d77a8e4eca7c5
Julian Hess <jhess@broadinstitute.org>
2019-12-13 10:21:38 -0500

    Need to wait for Slurm config files to be available

--- src/slurm_start.sh
+++ src/slurm_start.sh
@@ -4,5 +4,12 @@
 
 export SLURM_CONF=/mnt/nfs/clust_conf/slurm/slurm.conf
 
+echo -n "Waiting for Slurm configuration ..."
+while [ ! -f $SLURM_CONF ]; do
+	echo -n "."
+	sleep 1
+done
+echo
+
 sudo munged -f
-sudo -E slurmd -f /mnt/nfs/clust_conf/slurm/slurm.conf
+sudo -E slurmd -f $SLURM_CONF
