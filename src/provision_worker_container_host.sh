bb4058c32168faff04b7b1802748fc06006c00b9
Julian Hess <jhess@broadinstitute.org>
2020-01-10 16:55:44 +0000

    Worker container host needs to bind mount cgroups

--- src/provision_worker_container_host.sh
+++ src/provision_worker_container_host.sh
@@ -6,6 +6,6 @@ set -e
 /usr/local/share/cga_pipeline/src/nfs_provision_worker.sh ${1}-nfs
 
 # start Slurm docker
-docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs \
+docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
   --entrypoint /usr/local/share/cga_pipeline/src/docker_entrypoint_worker.sh --name slurm \
   broadinstitute/pydpiper
