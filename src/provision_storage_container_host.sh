61e37b2f3f81ba90d0aa2a0c29b441fe6e43605e
Julian Hess <jhess@broadinstitute.org>
2020-01-10 17:25:31 +0000

    Also update Docker run for storage node

--- src/provision_storage_container_host.sh
+++ src/provision_storage_container_host.sh
@@ -6,6 +6,6 @@ export CLOUDSDK_CONFIG=/etc/gcloud
 /usr/local/share/cga_pipeline/src/nfs_provision_server.sh $1 $2
 
 # start Slurm docker
-docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs \
-  --entrypoint /usr/local/share/cga_pipeline/src/slurm_start.sh --name slurm \
+docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
+  --entrypoint /usr/local/share/cga_pipeline/src/docker_entrypoint_worker.sh --name slurm \
   broadinstitute/pydpiper
