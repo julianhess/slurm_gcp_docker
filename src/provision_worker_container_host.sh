069c2aad9821e99bd08cc34c91b8c16323e48e2c
Julian Hess <jhess@broadinstitute.org>
2020-01-10 18:43:12 +0000

    Allow worker containers to control their host's Docker
    This lets us dispatch jobs that run in containers

--- src/provision_worker_container_host.sh
+++ src/provision_worker_container_host.sh
@@ -7,5 +7,6 @@ set -e
 
 # start Slurm docker
 docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
+  -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
   --entrypoint /usr/local/share/cga_pipeline/src/docker_entrypoint_worker.sh --name slurm \
   broadinstitute/pydpiper
