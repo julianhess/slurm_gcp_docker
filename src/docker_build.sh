5b441da17d5a103bcd33e4a9a1284bb6653dfb7a
Julian Hess <jhess@broadinstitute.org>
2020-01-13 22:46:42 +0000

    Make user and group match those of build host

--- src/docker_build.sh
+++ src/docker_build.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install nfs-common
 [ -d ~/.config/gcloud ] || { echo "gcloud has not yet been configured. Please run \`gcloud auth login'"; exit 1; }
 cp -r ~/.config/gcloud gc_conf
 sudo docker build -t broadinstitute/pydpiper:v0.1 -t broadinstitute/pydpiper:latest \
-  --build-arg USER=$USER --build-arg UID=$UID .
+  --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$(id -g) .
 rm -rf gc_conf
 
 #
