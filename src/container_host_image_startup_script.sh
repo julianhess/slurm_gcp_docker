1371b4be88431bcb352d354c41a8b1ad8af237e2
Julian Hess <jhess@broadinstitute.org>
2020-02-06 23:14:49 +0000

    TEMP: use stock branch when git cloning

--- src/container_host_image_startup_script.sh
+++ src/container_host_image_startup_script.sh
@@ -10,7 +10,7 @@ wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/doc
 sudo dpkg -i "containerd.io_1.2.6-3_amd64.deb" && \
 sudo dpkg -i "docker-ce-cli_19.03.3~3-0~ubuntu-disco_amd64.deb" && \
 sudo dpkg -i "docker-ce_19.03.3~3-0~ubuntu-disco_amd64.deb" && \
-sudo git clone https://github.com/getzlab/slurm_gcp_docker /usr/local/share/slurm_gcp_docker && \
+sudo git clone -b stock https://github.com/getzlab/slurm_gcp_docker /usr/local/share/slurm_gcp_docker && \
 sudo adduser $USER docker && \
 sudo ssed -R -i '/GRUB_CMDLINE_LINUX_DEFAULT/s/(.*)"(.*)"(.*)/\1"\2 cgroup_enable=memory swapaccount=1"\3/' /etc/default/grub && \
 sudo update-grub
