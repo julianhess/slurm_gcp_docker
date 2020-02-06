678fc36d10c55bf193bda516a4e9cb39ed69b1df
Julian Hess <jhess@broadinstitute.org>
2020-02-06 23:07:55 +0000

    Update setup.sh to use stock Docker image
    We no longer need to build a user-specific Docker image and push it to the
    image building VM; it's now built from a base image pulled from GCR and imbued
    with user-specific aspects (namely, UID/GID matching current machine) after
    being pulled from the container registry.

--- src/setup.sh
+++ src/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-./docker_build.sh && ./generate_container_host_image.sh k9-20-image
-docker stop registry
+# TODO: implement a better check for whether gcloud is properly configured
+#       simply checking for the existence of ~/.config/gcloud is insufficient
+[ -d ~/.config/gcloud ] || { echo "gcloud has not yet been configured. Please run \`gcloud auth login'"; exit 1; }
+
+sudo apt-get -y install nfs-common
+./generate_container_host_image.sh slurm-gcp-docker-v02
+
+sudo docker build -t broadinstitute/slurm_gcp_docker:v0.2 \
+  -t broadinstitute/slurm_gcp_docker:latest \
+  --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$(id -g) .
