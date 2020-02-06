13eb42e977f410664209af8866156f70d7f9d1af
Julian Hess <jhess@broadinstitute.org>
2020-02-06 23:06:11 +0000

    Bind mount /etc/gcloud

--- src/provision_storage_container_host.sh
+++ src/provision_storage_container_host.sh
@@ -8,5 +8,6 @@ export CLOUDSDK_CONFIG=/etc/gcloud
 # start Slurm docker
 docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
   -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
+  -v /etc/gcloud:/etc/gcloud \
   --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
   broadinstitute/pydpiper
