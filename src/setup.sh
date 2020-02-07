714ff9031ab501bc72364ef1bf89e1391bf9c11c
Julian Hess <jhess@broadinstitute.org>
2020-02-07 23:27:09 +0000

    Prompt user to authorize application default ...
    credentials and docker via gcloud

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,7 @@ set -e
 
 # TODO: implement a better check for whether gcloud is properly configured
 #       simply checking for the existence of ~/.config/gcloud is insufficient
-[ -d ~/.config/gcloud ] || { echo "gcloud has not yet been configured. Please run \`gcloud auth login'"; exit 1; }
+[ -d ~/.config/gcloud ] || { echo -e "gcloud has not yet been configured. Please run:\n * gcloud auth login\n * gcloud auth application-default login\n * gcloud auth docker"; exit 1; }
 
 sudo apt-get -y install nfs-common
 ./generate_container_host_image.sh slurm-gcp-docker-v0-2-`git rev-parse --short HEAD`
