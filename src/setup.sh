cdf25f44239ae8f7e8cf7eef1ce5ff1b1195d79a
Julian Hess <jhess@broadinstitute.org>
2020-02-07 13:06:53 +0000

    Automatically tag image with commit hash of HEAD

--- src/setup.sh
+++ src/setup.sh
@@ -5,7 +5,7 @@
 [ -d ~/.config/gcloud ] || { echo "gcloud has not yet been configured. Please run \`gcloud auth login'"; exit 1; }
 
 sudo apt-get -y install nfs-common
-./generate_container_host_image.sh slurm-gcp-docker-v02
+./generate_container_host_image.sh slurm-gcp-docker-v0-2-`git rev-parse --short HEAD`
 
 sudo docker build -t broadinstitute/slurm_gcp_docker:v0.2 \
   -t broadinstitute/slurm_gcp_docker:latest \
