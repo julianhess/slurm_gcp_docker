70e77a0a613dadb56db53d2e4331855470fcc52f
Jialin Ma <jma@broadinstitute.org>
2020-02-11 13:41:57 -0500

    Add --pid=host argument when starting the docker
    So that cgclassify can use the PID directly

--- src/provision_worker_container_host.sh
+++ src/provision_worker_container_host.sh
@@ -6,7 +6,7 @@ set -e
 /usr/local/share/slurm_gcp_docker/src/nfs_provision_worker.sh ${1}-nfs
 
 # start Slurm docker
-docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
+docker run -dti --rm --pid host --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
   -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
   -v /etc/gcloud:/etc/gcloud \
   --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
