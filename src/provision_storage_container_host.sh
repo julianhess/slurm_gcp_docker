d63bb493a312e50e34c52bcc3e3783af2ec19910
Jialin Ma <jma@broadinstitute.org>
2020-02-24 13:28:52 -0500

    Add missing --pid=host for docker run

--- src/provision_storage_container_host.sh
+++ src/provision_storage_container_host.sh
@@ -6,7 +6,7 @@ export CLOUDSDK_CONFIG=/etc/gcloud
 /usr/local/share/slurm_gcp_docker/src/nfs_provision_server.sh $1 $2 $3
 
 # start Slurm docker
-docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
+docker run -dti --rm --pid host --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
   -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
   -v /etc/gcloud:/etc/gcloud \
   --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
