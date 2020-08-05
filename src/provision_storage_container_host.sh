c3041d553324ee3ac7f981464e1a7b4e395309aa
Julian Hess <jhess@broadinstitute.org>
2020-08-05 13:09:43 -0400

    Allow devices on container host to be mounted within container

--- src/provision_storage_container_host.sh
+++ src/provision_storage_container_host.sh
@@ -6,8 +6,9 @@ export CLOUDSDK_CONFIG=/etc/gcloud
 /usr/local/share/slurm_gcp_docker/src/nfs_provision_server.sh $1 $2 $3 $4
 
 # start Slurm docker
-docker run -dti --rm --network host -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
+docker run -dti --rm --pid host --network host --privileged \
+  -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
   -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
-  -v /etc/gcloud:/etc/gcloud \
+  -v /etc/gcloud:/etc/gcloud -v /dev:/dev \
   --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
   broadinstitute/slurm_gcp_docker
