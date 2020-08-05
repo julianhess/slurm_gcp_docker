628a93425f0ae79c9bd8634d6c9a6371c7569e2d
Julian Hess <jhess@broadinstitute.org>
2020-08-05 13:17:56 -0400

    Consolidate code for starting container on workers

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
+docker run -dti --rm --pid host --network host --privileged \
+  -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
+  -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
+  -v /etc/gcloud:/etc/gcloud -v /dev:/dev \
+  --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
+  broadinstitute/slurm_gcp_docker
 
-# check if Slurm or Munge is already running; cannot start if this is the case
-pgrep slurmctld &> /dev/null && { echo -e "A Slurm controller is already running on this machine.\nPlease run \`[sudo] killall slurmctld\` and try again.";
-  exit 1; }
-pgrep slurmdbd &> /dev/null && { echo -e "The Slurm database daemon is already running on this machine.\nPlease run \`[sudo] killall slurmdbd\` and try again.";
-  exit 1; }
-pgrep munged &> /dev/null && { echo -e "Munge is already running on this machine.\nPlease run \`[sudo] killall munged\` and try again."; exit 1; }
-
-# check if mountpoint exists
-# TODO: update path to setup script in error message
-mountpoint -q /mnt/nfs || { echo "NFS is not mounted. Please run <setup script> again."; exit 1; }
-
-sudo docker run -v /mnt/nfs:/mnt/nfs --rm --network host -ti --name "pype_host" broadinstitute/pydpiper /bin/bash
--- src/provision_worker_container_host.sh
+++ src/provision_worker_container_host.sh
@@ -6,9 +6,4 @@ set -e
 /usr/local/share/slurm_gcp_docker/src/nfs_provision_worker.sh ${1}-nfs
 
 # start Slurm docker
-docker run -dti --rm --pid host --network host --privileged \
-  -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
-  -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
-  -v /etc/gcloud:/etc/gcloud -v /dev:/dev \
-  --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
-  broadinstitute/slurm_gcp_docker
+. /usr/local/share/slurm_gcp_docker/src/docker_run.sh
