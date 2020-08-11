617512cf86bcef3beab5b344e49ea2aacc8d0147
Julian Hess <jhess@broadinstitute.org>
2020-08-11 16:11:08 -0400

    Detach RO disks before shutdown

--- src/shutdown_worker_container_host.sh
+++ src/shutdown_worker_container_host.sh
@@ -3,3 +3,15 @@
 # alert controller that this host is being preempted; set status to fail
 docker exec slurm scontrol update nodename=$HOSTNAME state=FAIL reason="preempted" && \
 docker exec slurm scontrol update nodename=$HOSTNAME state=DOWN reason="preempted"
+
+#
+# detach any RO disks
+
+# get zone of instance
+export CLOUDSDK_CONFIG=/etc/gcloud
+ZONE=$(gcloud compute instances list --filter="name=${HOSTNAME}" \
+  --format='csv[no-heading](zone)')
+
+# detach all RO disks
+ls -1 /dev/disk/by-id/google-gsdisk* | grep -o 'gsdisk-.*$' | \
+  xargs -I {} -n 1 -P 0 gcloud compute instances detach-disk $HOSTNAME --device-name {} --zone $ZONE
