89fc12c8d943c226e11e7cd9eb3d6a8bcbdf410c
Julian Hess <jhess@broadinstitute.org>
2020-08-13 11:53:40 -0400

    Clarify errors in NFS mount

--- src/nfs_provision_worker.sh
+++ src/nfs_provision_worker.sh
@@ -5,21 +5,22 @@
 
 SHOST=$1
 
-# if the volume hasn't been bind mounted, then mount it as an NFS
-echo -n "Waiting for NFS to be ready ..."
+echo "Starting NFS ..."
 
 # check if mount is stale
 timeout 30 stat -t /mnt/nfs &> /dev/null
 EC=$?
 if [[ $EC == 124 ]]; then
 	# attempt to unmount
+	echo "NFS mount is stale; attempting unmount ..." > /dev/stderr
 	if ! sudo timeout 2 umount -f /mnt/nfs; then
-		echo -e "\nNFS mount is stale. Please close any open files (check with \`lsof -b | grep /mnt/nfs\`) and then \`sudo umount -f /mnt/nfs\`." > /dev/stderr
+		echo "Unmount failed. Please close any open files (check with \`lsof -b | grep /mnt/nfs\`) and then \`sudo umount -f /mnt/nfs\`." > /dev/stderr
 		exit 1
 	fi
 fi
 
 # otherwise, wait for mount to be ready (NFS server is starting up)
+echo -n "Waiting for NFS server to be ready ..."
 [ ! -d /mnt/nfs ] && sudo mkdir -p /mnt/nfs
 while ! mountpoint -q /mnt/nfs; do
 	sudo mount -o defaults,hard,intr ${SHOST}:/mnt/nfs /mnt/nfs &> /dev/null
