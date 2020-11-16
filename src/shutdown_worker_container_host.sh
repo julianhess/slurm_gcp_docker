befd30601ce17ef0ac70f2ee9159060625cb8738
Julian Hess <jhess@broadinstitute.org>
2020-11-16 17:29:47 -0500

    Clarify shutdown reason

--- src/shutdown_worker_container_host.sh
+++ src/shutdown_worker_container_host.sh
@@ -3,7 +3,7 @@
 # alert controller that this host is being preempted; set status to fail
 docker exec slurm scontrol update nodename=$HOSTNAME state=FAIL reason="preempted" && \
 docker exec slurm scontrol update nodename=$HOSTNAME state=DOWN reason="preempted" && \
-docker exec slurm scontrol update nodename=$HOSTNAME state=POWER_DOWN reason="eligible"
+docker exec slurm scontrol update nodename=$HOSTNAME state=POWER_DOWN reason="powerdown"
 
 #
 # detach any RO disks
