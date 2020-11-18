0d4cc3c1692fe80499f7f0e4a94238abe26814c9
Julian Hess <jhess@broadinstitute.org>
2020-11-18 15:52:50 -0500

    Increase maxkeys/maxbytes

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -1,7 +1,12 @@
+# resolves occasional quota exceeded error, per https://stackoverflow.com/questions/54405454/error-response-from-daemon-join-session-keyring-create-session-key-disk-quota
+# these values come from root_max{keys,bytes}
+echo 1000000 > /proc/sys/kernel/keys/maxkeys
+echo 25000000 > /proc/sys/kernel/keys/maxbytes
+
+# start the container
 docker run -dti --rm --pid host --network host --privileged \
   -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
   -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
   -v /etc/gcloud:/etc/gcloud -v /dev:/dev \
   --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
   broadinstitute/slurm_gcp_docker
-
