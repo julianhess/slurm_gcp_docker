0ac2754ba86d83a04fa2382ee573622db9c4ca37
Jialin Ma <jma@broadinstitute.org>
2021-02-08 11:47:26 +0000

    On controller build docker image from local base image if exists

---
+++ src/docker_build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION=$(cat VERSION)
+
+## Choose the docker base image to build from:
+## 1. (For developer) If broadinstitute/slurm_gcp_docker_base:$VERSION exists locally, use it.
+##    This is the local image built by docker_build_base.sh
+## 2. Otherwise use gcr.io/broad-getzlab-workflows/slurm_gcp_docker_base:$VERSION
+
+if sudo docker image inspect broadinstitute/slurm_gcp_docker_base:$VERSION >/dev/null 2>/dev/null; then
+    echo "Building docker image from broadinstitute/slurm_gcp_docker_base:$VERSION (local)"
+    echo docker_base_image=broadinstitute/slurm_gcp_docker_base:$VERSION
+else
+    echo "Building docker image from gcr.io/broad-getzlab-workflows/slurm_gcp_docker_base:$VERSION"
+    echo docker_base_image=gcr.io/broad-getzlab-workflows/slurm_gcp_docker_base:$VERSION
+fi
+
+sudo docker build -t broadinstitute/slurm_gcp_docker:$VERSION \
+  -t broadinstitute/slurm_gcp_docker:latest \
+  --build-arg HOST_USER=$USER --build-arg UID=$UID --build-arg GID=$(id -g) \
+  --build-arg DOCKER_BASE_IMAGE=$docker_base_image \
+  .
