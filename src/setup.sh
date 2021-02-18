832695c6162ebe47af7a4e312affe57f852056b6
Jialin Ma <jma@broadinstitute.org>
2021-02-18 07:56:26 +0000

    setup.sh fallback to setup_local.py

--- src/setup.sh
+++ src/setup.sh
@@ -2,13 +2,5 @@
 
 set -e
 
-# TODO: implement a better check for whether gcloud is properly configured
-#       simply checking for the existence of ~/.config/gcloud is insufficient
-[ -d ~/.config/gcloud ] || { echo -e "gcloud has not yet been configured. Please run:\n * gcloud auth login\n * gcloud auth application-default login\n * gcloud auth docker"; exit 1; }
-
-sudo apt-get -y install nfs-common
-
-VERSION=$(cat VERSION)
-IVERSION=$(tr . - <<< $VERSION)
-
-./docker_build.sh && ./generate_container_host_image.py -i slurm-gcp-docker-${IVERSION}-`git rev-parse --short HEAD`-$USER
+## Currently just fall back to setup_local.py
+./setup_local.py
