82ec5eb0cb8764344043f5579ae9b6e2cb7727fa
Jialin Ma <jma@broadinstitute.org>
2021-02-25 15:41:09 +0000

    Revert "Move slurm.conf inside of docker"
    This reverts commit d53735e6e84ba72bb91ed811ac12a15cdc06479a.

--- src/docker_entrypoint_controller.sh
+++ src/docker_entrypoint_controller.sh
@@ -6,6 +6,5 @@ sudo -E /usr/local/share/slurm_gcp_docker/src/docker_copy_gcloud_credentials.sh
 
 sudo mysqld &
 /usr/local/share/slurm_gcp_docker/src/provision_server.py
-# export SLURM_CONF=/usr/local/etc/slurm.conf # In Dockerfile, we used --sysconfdir=/usr/local/etc, we probably don't need to set this environment
-
+export SLURM_CONF=/mnt/nfs/clust_conf/slurm/slurm.conf
 /bin/bash
--- src/docker_run.sh
+++ src/docker_run.sh
@@ -1,8 +1,3 @@
-
-if [ "$SLURMCTL_HOST" = "" ]; then
-  echo "Missing SLURMCTL_HOST environment variable" && exit 1
-fi
-
 # resolves occasional quota exceeded error, per https://stackoverflow.com/questions/54405454/error-response-from-daemon-join-session-keyring-create-session-key-disk-quota
 # these values come from root_max{keys,bytes}
 echo 1000000 > /proc/sys/kernel/keys/maxkeys
@@ -13,6 +8,5 @@ docker run -dti --rm --pid host --network host --privileged \
   -v /mnt/nfs:/mnt/nfs -v /sys/fs/cgroup:/sys/fs/cgroup \
   -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
   -v /etc/gcloud:/etc/gcloud -v /dev:/dev \
-  --env SLURMCTL_HOST="$SLURMCTL_HOST" \
   --entrypoint /usr/local/share/slurm_gcp_docker/src/docker_entrypoint_worker.sh --name slurm \
   broadinstitute/slurm_gcp_docker
