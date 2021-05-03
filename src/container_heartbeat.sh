d13fc940d3426b879c023ff56015cef673c3ae53
Julian Hess <jhess@broadinstitute.org>
2021-05-03 11:16:55 -0400

    Delete hosts that flatline in heartbeat script

--- src/container_heartbeat.sh
+++ src/container_heartbeat.sh
@@ -3,17 +3,23 @@
 # runs inside each worker container, checks every 5 minutes if the container is
 # healthy. if not, blacklist this node.
 
+# get zone of instance
+ZONE=$(gcloud compute instances list --filter="name=${HOSTNAME}" \
+  --format='csv[no-heading](zone)')
+
 while true; do
 	# check if Podman is responsive
 	if ! timeout 30 podman info; then
 		scontrol update nodename=$HOSTNAME state=FAIL reason="podman flatlined" && \
-		scontrol update nodename=$HOSTNAME state=DOWN reason="podman flatlined"
+		scontrol update nodename=$HOSTNAME state=DOWN reason="podman flatlined" && \
+		gcloud compute instances delete $HOSTNAME --zone $ZONE --quiet
 	fi
 
 	# check if disk is full (<5% space remaining on root partition)
 	if ! df / | awk 'NR == 2 { if($4/($4 + $3) < 0.05) { exit 1 } }'; then
 		scontrol update nodename=$HOSTNAME state=FAIL reason="local disk full" && \
-		scontrol update nodename=$HOSTNAME state=DOWN reason="local disk full"
+		scontrol update nodename=$HOSTNAME state=DOWN reason="local disk full" && \
+		gcloud compute instances delete $HOSTNAME --zone $ZONE --quiet
 	fi
 
 	sleep 300
