4778f97ed3b0f58249c10843d3db54cfd9a74f28
Jialin Ma <jma@broadinstitute.org>
2021-05-26 14:53:45 +0000

    setup_remote.py

--- src/install_service.py
+++ src/install_service.py
@@ -6,11 +6,13 @@ services_dir = os.path.join(os.path.dirname(__file__), "services")
 
 prefectserver = os.path.join(services_dir, "prefectserver.service")
 caninebackend = os.path.join(services_dir, "caninebackend.service")
+jupyternotebook = os.path.join(services_dir, "jupyternotebook.service")
 
 def main():
     subprocess.check_call(["sudo", "cp", prefectserver, "/etc/systemd/system/prefectserver.service"])
     subprocess.check_call(["mkdir", "-p", os.path.expanduser("~/.config/systemd/user")])
     subprocess.check_call(["cp", caninebackend, os.path.expanduser("~/.config/systemd/user/caninebackend.service")])
+    subprocess.check_call(["cp", jupyternotebook, os.path.expanduser("~/.config/systemd/user/jupyternotebook.service")])
 
     subprocess.check_call(["mkdir", "-p", os.path.expanduser("~/.prefect")])
     with open(os.path.expanduser("~/.prefect/backend.toml"), "w") as f:
